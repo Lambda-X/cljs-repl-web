40eeb0ae69d9982b957aeb1b8f27389f1e911dcc
Andrea Richiardi <a.richiardi.work@gmail.com>
2015-10-26 10:48:40 -0700

    Doc string changes.

--- src/cljs/cljs_bootstrap/core.cljs
+++ src/cljs/cljs_bootstrap/core.cljs
@@ -4,42 +4,39 @@
             [cljs-bootstrap.repl :as repl]
             [cljs-bootstrap.common :as common]))
 
-(defn ^:export read-eval-print
-  "Reads evaluates and prints the input source.
+(defn ^:export read-eval-call
+  "Reads, evaluates and calls back with the evaluation result.
 
-  The first parameter is a map of configuration options that override
-  the ones given in init-repl. The supported options are the same.
+  The first parameter is a map of configuration options, currently
+  supporting:
 
-  The second parameter, callback, is a function (fn [success, result]
-  ...) where success is a boolean indicating if everything went right
-  and result will contain the actual result of the evaluation as string
-  or the error as js/Error.
+  * `:verbose` will enable the the evaluation logging, defaults to false.
+
+  The second parameter `cb`, should be a 1-arity function which receives
+  the result map.
+
+  Therefore, given callback = `(fn [result] ...)`, the main map keys are:
+
+  ```
+  { :success? ;; a boolean indicating if everything went right
+    :value    ;; (if (success? result)) will contain the actual yield of the evaluation
+    :error    ;; (if (not (success? result)) will contain a js/Error }
+  ```
 
   It initializes the repl harness if necessary."
   ([callback source] (repl/read-eval-print {} callback source))
   ([opts callback source] (repl/read-eval-print opts callback source)))
 
-(def ^:export rep
-  "Reads evaluates and prints the input source.
-
-  The first parameter is a map of configuration options that override
-  the ones given in init-repl. The supported options are the same.
-
-  The second parameter, callback, is a function (fn [success, result]
-  ...) where success is a boolean indicating if everything went right
-  and result will contain the actual result of the evaluation as string
-  or the error as js/Error.
-
-  It initializes the repl harness if necessary."
-  read-eval-print)
-
-(defn ^:export get-prompt []
+(defn ^:export get-prompt
+  "Retrieves the repl prompt to display, according to the current
+  namespace. Returns a string."
+  []
   (str (repl/current-ns) "=> "))
 
 (defn ^:export exception->str
-  "Return the message string of the input exception."
-  ([ex] (exception->str ex false))
-  ([ex print-stack?]
+  "Return the message string of the input `js/Error`."
+  ([error] (exception->str error false))
+  ([error print-stack?]
    (str (common/extract-message ex)
         (when (and ex print-stack?)
           (str "\n" (.-stack ex))))))
