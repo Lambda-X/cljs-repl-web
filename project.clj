f22ad06aea19ffbbc194647cfcfcd35ae2065da8
Andrea Richiardi <a.richiardi.work@gmail.com>
2015-11-25 08:09:50 -0800

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-repl-web "0.1.1-SNAPSHOT"
+(defproject cljs-repl-web "0.1.1"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
                  [replumb/replumb "0.1.2"]
