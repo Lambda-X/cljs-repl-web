79fe910ab4d9e879a66e1fddf77f2c124b626b22
Tomasz Biernacki <tomasz.biernacki@scalac.io>
2015-11-26 19:17:57 +0100

    Focus console on page load. Fixes #135.

--- src/cljs/cljs_repl_web/views.cljs
+++ src/cljs/cljs_repl_web/views.cljs
@@ -29,7 +29,8 @@
    (fn []
      (let [jqconsole (cljs/cljs-console! console-opts)]
        (dispatch [:add-console :cljs-console jqconsole])
-       (cljs/cljs-console-prompt! jqconsole)))))
+       (cljs/cljs-console-prompt! jqconsole)
+       (console/focus-console! jqconsole)))))
 
 (defn cljs-console-render []
   [:div.cljs-console.console])
