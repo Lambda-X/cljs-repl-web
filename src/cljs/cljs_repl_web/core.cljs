7bc0b1faf6451ade0a39238753abac943cb26f4a
Tomasz Biernacki <tomasz.biernacki@scalac.io>
2015-12-10 00:38:30 +0100

    Move cache downloading to handler

--- src/cljs/cljs_repl_web/core.cljs
+++ src/cljs/cljs_repl_web/core.cljs
@@ -3,7 +3,6 @@
             [re-frame.core :refer [dispatch dispatch-sync]]
             [re-com.core :refer [p h-box v-box box gap line]]
             [devtools.core :as devtools]
-            [cljs-repl-web.cache :as cache]
             [cljs-repl-web.handlers]
             [cljs-repl-web.subs]
             [cljs-repl-web.views :as views]))
@@ -17,12 +16,6 @@
 
 (defn ^:export main []
   (println "In cljs-browser-repl.core/main")
-
-  ;; we load the cljs core cache manually in order to reduce the app size
-  ;; see https://github.com/clojure/clojurescript/wiki/Optional-Self-hosting for more info
-  ;; see also related issue in replbum https://github.com/ScalaConsultants/replumb/issues/42
-  (cache/get-cljs-core-cache)
-
   (dispatch-sync [:initialize])
   (reagent/render [views/repl-component] (.getElementById js/document "app-center"))
   (reagent/render [views/bottom-panel] (.getElementById js/document "app-bottom"))
